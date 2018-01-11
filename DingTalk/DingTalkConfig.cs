e8159034beeac0eb3fb897e382e0dec6fb696562
634247979 <634247979@qq.com>
2018-01-11 17:02:30 +0800

    123

--- DingTalk/DingTalkConfig.cs
+++ DingTalk/DingTalkConfig.cs
@@ -8,6 +8,9 @@ namespace DingTalkServer
 {
     public class DingTalkConfig
     {
+
+
+
         public string CorpId { get; set; } = "ding1238d49a88c92de535c2f4657eb6378f";
         public string CorpSecret { get; set; } = "JEgMvDPWMWtJM2ZVzvup73_XBHsI-lL-lflOK_E1rW16PGVICvSkSrRDIZkuJbAT";
         public string AccessToken { get; set; } = ConfigurationManager.AppSettings["AccessToken"].ToString();
