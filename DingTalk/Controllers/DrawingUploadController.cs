86e31c831cda890a39c944f7ad35bc9d2bbedf6b
caixingtong <634247979@qq.com>
2018-04-08 17:10:07 +0800

    新增Excel读取接口

--- DingTalk/Controllers/DrawingUploadController.cs
+++ DingTalk/Controllers/DrawingUploadController.cs
@@ -1,7 +1,9 @@
-﻿using DingTalk.Models;
+﻿using Common.Excel;
+using DingTalk.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,6 +20,11 @@ namespace DingTalk.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 文件上传接口
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
         [HttpPost]
         public string Upload(FormCollection form)
         {
@@ -45,7 +52,7 @@ namespace DingTalk.Controllers
             {
                 //文件大小不为0
                 HttpPostedFileBase files = Request.Files[0];
-                string newFileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx";
+                string newFileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
                 files.SaveAs(Server.MapPath(@"~\UploadFile\Excel\"+ newFileName));
 
                 return JsonConvert.SerializeObject(new ErrorModel
@@ -55,6 +62,21 @@ namespace DingTalk.Controllers
                     Content= Server.MapPath(@"~\UploadFile\Excel\" + newFileName)
                 });
             }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <returns></returns>
+        /// 测试数据/DrawingUpload/LoadExcel?path=C:\\Users\\tong\\Source\\Repos\\DingTalk\\DingTalk\\UploadFile\\Excel\\20180408163044.xlsx
+        [HttpGet]
+        public string LoadExcel(string Path)
+        {
+            //Path = "C:\\Users\\tong\\Source\\Repos\\DingTalk\\DingTalk\\UploadFile\\Excel\\20180408164559.xls";
+            DataTable db= ExcelHelperByNPOI.ImportExcel2003toDt(Path);
+            return JsonConvert.SerializeObject(db);
         }
 
     }
