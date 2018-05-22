3fe45460c222996c2feb55522e0c978367c7dbe5
634247979 <634247979@qq.com>
2018-05-22 08:28:17 +0800

    修改图纸上传

---
+++ DingTalk/Models/DbModels/PurchaseProcedureInfo.cs
@@ -0,0 +1,28 @@
+namespace DingTalk.Models.DbModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PurchaseProcedureInfo")]
+    public partial class PurchaseProcedureInfo
+    {
+        [Column(TypeName = "numeric")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal Id { get; set; }
+
+        [StringLength(300)]
+        public string DrawingNo { get; set; }
+
+        [StringLength(300)]
+        public string ProcedureInfoId { get; set; }
+
+        [StringLength(300)]
+        public string CreateManId { get; set; }
+
+        [StringLength(300)]
+        public string CreateTime { get; set; }
+    }
+}
