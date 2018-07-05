ee37e9235eb2b42356d467d9df361a8e371a5247
634247979 <634247979@qq.com>
2018-07-05 15:20:31 +0800

    修改GetFileMsg接口（路径直接写死）

--- DingTalk/Controllers/ProjectController.cs
+++ DingTalk/Controllers/ProjectController.cs
@@ -187,13 +187,11 @@ namespace DingTalk.Controllers
         /// <summary>
         /// 获取项目目录下的文件信息
         /// </summary>
-        /// <param name="Path">绝对路径</param>
         /// <returns></returns>
-        /// 测试数据：/Project/GetFileMsg?Path=E:\Project\DingTalk\DingTalk\UploadFile\ProjectFile
         [HttpGet]
-        public string GetFileMsg(string Path)
+        public string GetFileMsg()
         {
-            //string Path = string.Format(@"{0}UploadFile\Excel", AppDomain.CurrentDomain.BaseDirectory);           
+            string Path = string.Format(@"{0}UploadFile\ProjectFile", AppDomain.CurrentDomain.BaseDirectory);
             return JsonConvert.SerializeObject(FileHelper.GetFileNames(Path));
         }
 
