c626f86014411a58d68eb78d2816a5502422e374
wuliao522 <18021071510@163.com>
2018-08-03 09:16:16 +0800

    公车申请

--- DingTalk/Scripts/lib.js
+++ DingTalk/Scripts/lib.js
@@ -132,6 +132,9 @@ function _getTime() {
     var second = d.getSeconds()
     if (month < 10) month = '0' + month
     if (day < 10) day = '0' + day
+    if (hour < 10) hour = '0' + hour
+    if (minute < 10) minute = '0' + minute
+    if (second < 10) second = '0' + second
     return year + split + month + split + day + ' ' + hour + ':' + minute + ':' + second
 }
 
