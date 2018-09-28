4f443587a91bd4d49912b7221576b69a2150c72e
wuliao522 <18021071510@163.com>
2018-09-28 19:49:46 +0800

    1

--- DingTalk/Scripts/lib.js
+++ DingTalk/Scripts/lib.js
@@ -994,7 +994,7 @@ Vue.component('sam-approver-list', {
                                         :class="{'el-tag--danger':node.IsBack}"
                                         style="width:60px;text-align:center;"
                                         >
-                                    {{p.substring(0,3)}}
+                                    {{p}}
                                 </el-tag>
                                 
                                 <span v-if="(node.NodeName=='抄送' || !node.ApplyTime) && a < node.NodePeople.length-1">,</span>
@@ -1014,7 +1014,7 @@ Vue.component('sam-approver-list', {
                                         :type="node.ApplyTime?'success':''"
                                         style="width:60px;text-align:center;"
                                         >
-                                    {{ap.name.substring(0,3)}}
+                                    {{ap.name}}
                                 </el-tag>
                             </template>
 
