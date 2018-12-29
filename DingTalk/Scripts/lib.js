e3b7033926f318bf09b519b007343796c6085915
wuliao522 <18021071510@163.com>
2018-12-29 10:03:57 +0800

    111

--- DingTalk/Scripts/lib.js
+++ DingTalk/Scripts/lib.js
@@ -1035,6 +1035,9 @@ var mixin = {
                     }
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    console.log(url)
+                    console.log(param)
+                    console.log('报错信息！！！！！！！！！！！！！！！！！！！！')
                     console.log(XMLHttpRequest);
                 }
             })
